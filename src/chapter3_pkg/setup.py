78fda4dd98e276195912fb6bb7adbf50cc8e783d
tsingsesu <tsingsesu@163.com>
2026-08-27 16:23:02 +0800

    827下午任务3基本完成

--- src/chapter3_pkg/setup.py
+++ src/chapter3_pkg/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'chapter3_pkg'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +30,9 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'publisher = chapter3_pkg.publisher:main',
+            'subscriber = chapter3_pkg.subscriber:main',
+
         ],
     },
 )
